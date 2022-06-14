79727610b11e997c8dbb0d57573503615790b6c6
soomin JI <37145674+tnals5152@users.noreply.github.com>
2022-06-14 22:24:26 +0900

    Create 1480. Running Sum of 1d Array.py

---
+++ leetcode/easy/1480. Running Sum of 1d Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for index, num in enumerate(nums[1:]):
+            nums[index + 1] += nums[index]
+            
+        return nums
